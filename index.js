c2dd4990d698c1f438ca9a938443667d18b7f077
Your Name <you@example.com>
2026-03-03 22:52:28 +0800

    Midway

---
+++ index.js
@@ -0,0 +1,17 @@
+const express =  require('express');
+const app = express();
+const port = 3000;
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log('Server running at http://localhost:${port}');
+});
