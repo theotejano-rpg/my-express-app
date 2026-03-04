64fa24cdaff9e81266851def1cf40a702e25690e
Your Name <you@example.com>
2026-03-04 21:52:09 +0800

    Saon pag fix ani oiyps

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const express =  require('express');
 const app = express();
 const port = 3000;
 
-app.get('/about', (req, res) => {
-    res.send('About Us');
-});
-
 app.use(express.static('public'));
+
+app.use(express.json());
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About Us');
+});
+
 app.listen(port, () => {
-    console.log('Server running at http://localhost:${port}');
+    console.log(`Server running at http://localhost:${port}`);
 });
